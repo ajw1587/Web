20a2276d958287a7ed40c944cf03566bad00b1d2
iwhwang0503 <iwhwang0503@gmail.com>
2025-10-29 17:51:01 +0900

    React 9강 차례

--- React/React-Basic/src/components/common/navigation/CommonNav.tsx
+++ React/React-Basic/src/components/common/navigation/CommonNav.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import styles from "@components/common/navigation/CommonNav.module.scss"
+import navJson from './nav.json'
 
 interface Navigation {
   index: number
@@ -11,107 +13,22 @@ interface Navigation {
 
 function CommonNav() {
 
-  const [navigation, setNavigation] = useState([
-    {
-      index: 0,
-      path: '/edit',
-      label: '보도/편집 전용',
-      searchValue: 'edit',
-      isActive: false
-    },
-    {
-      index: 1,
-      path: '/following',
-      label: '팔로잉',
-      searchValue: 'following',
-      isActive: false
-    },
-    {
-      index: 2,
-      path: '/photoPlus',
-      label: 'Unsplash Photo+',
-      searchValue: 'photo',
-      isActive: false
-    },
-    {
-      index: 3,
-      path: '/oneColor',
-      label: '단색',
-      searchValue: 'one color',
-      isActive: false
-    },
-    {
-      index: 4,
-      path: '/3dRender',
-      label: '3D 렌더링',
-      searchValue: '3d rendering',
-      isActive: false
-    },
-    {
-      index: 5,
-      path: '/nature',
-      label: '자연',
-      searchValue: 'nature',
-      isActive: false
-    },
-    {
-      index: 6,
-      path: '/texture',
-      label: '텍스쳐 및 패턴',
-      searchValue: 'texture',
-      isActive: false
-    },
-    {
-      index: 7,
-      path: 'interior',
-      label: '인테리어',
-      searchValue: 'interior',
-      isActive: false
-    },
-    {
-      index: 8,
-      path: '/film',
-      label: '필름',
-      searchValue: 'film',
-      isActive: false
-    },
-    {
-      index: 9,
-      path: '/experimental',
-      label: '실험적인',
-      searchValue: 'experimental',
-      isActive: false
-    },
-    {
-      index: 10,
-      path: '/travel',
-      label: '여행',
-      searchValue: 'travel',
-      isActive: false
-    },
-    {
-      index: 11,
-      path: '/sports',
-      label: '스포츠',
-      searchValue: 'sports',
-      isActive: false
-    },
-  ])
+  const [navigation, setNavigation] = useState(navJson)
 
   // useState를 선언한 반응성을 가진 데이터를 기반으로 UI를 반복 호출
   const navLinks = navigation.map((item: Navigation) => {
     return (
-      <div className={styles.navigation__menu}>
+      <Link to={item.path} className={styles.navigation__menu}>
         <span className={styles.navigation__menu__label} key={item.path}>
           {item.label}
         </span>
-      </div>
+      </Link>
     )
   })
   return (
-    <div className={styles.navigation}>
+    <nav className={styles.navigation}>
       {navLinks}
-    </div>
+    </nav>
   )
 }
 
--- React/React-Basic/src/pages/index/index.tsx
+++ React/React-Basic/src/pages/index/index.tsx
@@ -1,7 +1,10 @@
 import CommonHeader from '@components/common/header/CommonHeader'
 import CommonSearchBar from '@components/common/searchBar/CommonSearchBar'
 import CommonNav from '@components/common/navigation/CommonNav'
+import CommonFooter from '@/components/common/footer/CommonFooter'
+// CSS
 import styles from "./styles/index.module.scss"
+import Card from './components/Card'
 
 function index() {
   return (
@@ -22,9 +25,16 @@ function index() {
             <CommonSearchBar />
           </div>
         </div>
-        <div className={styles.page__contents__imageBox}></div>
+        <div className={styles.page__contents__imageBox}>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
       </div>
       {/* 공통부터 UI 부분 */}
+      <CommonFooter />
     </div>
   )
 }
